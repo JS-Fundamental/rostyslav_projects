77bbe34041e949d8a8e4543c542994aacaf83eb7
Rostyslav Klyba <rklyba@gmail.com>
2023-01-09 19:59:04 +0200

    Removed unnecessary comments

--- hw_5/task_2/app.js
+++ hw_5/task_2/app.js
@@ -1,6 +1,3 @@
-/*
-2) створити блок із списком веб-фреймворків(лише їх назви). При кліку на певний фреймворк у блоці фото (який буде з права від списку фреймворків) буде відображатись фото конкретного фреймворку, фото вибрати із мережі.
-*/
 const blockOfPhotos = document.querySelector('.photo-block');
 const imgTag = document.querySelector('img');
 const liTags = document.querySelectorAll('li');
